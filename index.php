199207d32eff58ac214b45937ae0bd380a5a2b11
Ivan Mandić <ivan.mandic@predavaci.algebra.hr>
2023-03-01 20:11:20 +0000

    Operatori vjezba

--- index.php
+++ index.php
@@ -1,4 +1,22 @@
 <?php
 
-require './about.php';
-require_once './about.php';
+$a = 10;
+$b = 20;
+$c = 'Prva rijec';
+$d = ' druga rijec';
+
+echo 'a+b= ', $a + $b, "\n";
+echo 'a-b= ', $a - $b, "\n";
+echo 'a*b= ', $a * $b, "\n";
+echo 'a/b= ', $a / $b, "\n";
+echo 'a%b= ', $a % $b, "\n";
+
+$f = $c . '-' . $d;
+echo "Konkatenacija: ", $f, "\n\n";
+
+echo $a += $b, "\n";
+
+var_dump($a > $b);
+
+echo $a++, "\n";
+echo $b--, "\n";
