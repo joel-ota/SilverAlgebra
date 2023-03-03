952c78bbbd09601119c1ba1f1991d2612fd29dd7
Ivan Mandić <ivan.mandic@predavaci.algebra.hr>
2023-03-03 19:13:38 +0000

    Vježbe 2 iz nizova

--- index.php
+++ index.php
@@ -1,22 +1,33 @@
 <?php
 
 // Definiranje niza
-$primeNumbers = [2,3,5,7,11];
-// Provjera trećeg elementa u nizu i ispis
-var_dump(isset($primeNumbers[2]));
-// Ispis trećeg elementa zato što postoji u nizu
-if (isset($primeNumbers[2])){
-    echo "\n", "Postoji ", $primeNumbers[2], "\n";
-} else{
-    echo "\n", "Ne postoji treci element.", "\n";
-} 
-// Dodavanje novog elemementa na kraj niza
-$primeNumbers[] = 13;
-// Ispis broja elemenata u nizu
-echo count($primeNumbers);
-// Ispis cijelog niza
-print_r($primeNumbers);
-// Sortiranje niza silazno po vrijednostima
-rsort($primeNumbers);
-// Ispis cijelog niza
-print_r($primeNumbers);
+$users = [
+    [
+        'name' => 'Ivan',
+        'surname' => 'Horvat',
+        'age' => 33,
+        'sex' => 'M'
+    ],
+    [
+        'name' => 'Mia',
+        'surname' => 'Mikulić',
+        'age' => 21,
+        'sex' => 'F'
+    ]
+];
+// Ispis niza
+print_r($users);
+// Brisanje ključa godine i njegove vrijednosti za svakog korisnika
+unset($users[0]['sex']);
+unset($users[1]['sex']);
+// Ispis niza
+print_r($users);
+// Dodavanje novog korisnika u niz
+$users[] = [
+    'name' => 'Marko',
+    'surname' => 'Antić',
+    'age' => 45
+];
+// Ispis niza
+print_r($users);
+
