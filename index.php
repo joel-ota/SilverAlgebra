ce5e2e0a84c0ed07ef6cd8954b7d4fd9cb85d094
Ivan Mandić <ivan.mandic@predavaci.algebra.hr>
2023-03-03 18:52:11 +0000

    Vježba 1 s nizovima

--- index.php
+++ index.php
@@ -1,22 +1,22 @@
 <?php
 
-$a = 10;
-$b = 20;
-$c = 'Prva rijec';
-$d = ' druga rijec';
-
-echo 'a+b= ', $a + $b, "\n";
-echo 'a-b= ', $a - $b, "\n";
-echo 'a*b= ', $a * $b, "\n";
-echo 'a/b= ', $a / $b, "\n";
-echo 'a%b= ', $a % $b, "\n";
-
-$f = $c . '-' . $d;
-echo "Konkatenacija: ", $f, "\n\n";
-
-echo $a += $b, "\n";
-
-var_dump($a > $b);
-
-echo $a++, "\n";
-echo $b--, "\n";
+// Definiranje niza
+$primeNumbers = [2,3,5,7,11];
+// Provjera trećeg elementa u nizu i ispis
+var_dump(isset($primeNumbers[2]));
+// Ispis trećeg elementa zato što postoji u nizu
+if (isset($primeNumbers[2])){
+    echo "\n", "Postoji ", $primeNumbers[2], "\n";
+} else{
+    echo "\n", "Ne postoji treci element.", "\n";
+} 
+// Dodavanje novog elemementa na kraj niza
+$primeNumbers[] = 13;
+// Ispis broja elemenata u nizu
+echo count($primeNumbers);
+// Ispis cijelog niza
+print_r($primeNumbers);
+// Sortiranje niza silazno po vrijednostima
+rsort($primeNumbers);
+// Ispis cijelog niza
+print_r($primeNumbers);
