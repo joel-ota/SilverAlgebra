5407b4e09fcf44f098ebfd8acf3dd7ced09bfb95
Ivan Mandić <ivan.mandic@predavaci.algebra.hr>
2023-03-07 19:07:49 +0000

    Vjezba uvjetne strukture

--- index.php
+++ index.php
@@ -1,33 +1,41 @@
 <?php
 
-// Definiranje niza
-$users = [
-    [
-        'name' => 'Ivan',
-        'surname' => 'Horvat',
-        'age' => 33,
-        'sex' => 'M'
-    ],
-    [
-        'name' => 'Mia',
-        'surname' => 'Mikulić',
-        'age' => 21,
-        'sex' => 'F'
-    ]
-];
-// Ispis niza
-print_r($users);
-// Brisanje ključa godine i njegove vrijednosti za svakog korisnika
-unset($users[0]['sex']);
-unset($users[1]['sex']);
-// Ispis niza
-print_r($users);
-// Dodavanje novog korisnika u niz
-$users[] = [
-    'name' => 'Marko',
-    'surname' => 'Antić',
-    'age' => 45
-];
-// Ispis niza
-print_r($users);
+// Definiranje varijabli
+$a = 5;
+$b = 10;
+$c = 15;
 
+// Provjera da li je vrijednost varijable b između a i c
+if(($b > $a && $b < $c) || ($b < $a && $b > $c)){
+    echo 'Vrijednost b je između a i c';
+} else {
+    echo 'Vrijednost b nije između a i c';
+}
+
+// Provjera i ispis rezultata
+switch(date('N')){
+    case 1:
+        echo 'Danas je ponedjeljak.';
+        break;
+    case 2:
+        echo 'Danas je utorak.';
+        break;
+    case 3:
+        echo 'Danas je srijeda.';
+        break;
+    case 4:
+        echo 'Danas je četvrtak.';
+        break;
+    case 5:
+        echo 'Danas je petak.';
+        break;
+    case 6:
+        echo 'Danas je subota.';
+        break;
+    case 7:
+        echo 'Danas je nedjelja.';
+        break;
+    default:
+        echo 'Nepostojeći dan.';
+        break;
+}
