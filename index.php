53f805f309e829e5c6b3e5d454ac97fd5897b56f
Ivan Mandić <ivan.mandic@predavaci.algebra.hr>
2023-03-07 19:09:02 +0000

    Vjezbe iz petlji

--- index.php
+++ index.php
@@ -1,41 +1,16 @@
 <?php
 
-// Definiranje varijabli
-$a = 5;
-$b = 10;
-$c = 15;
-
-// Provjera da li je vrijednost varijable b između a i c
-if(($b > $a && $b < $c) || ($b < $a && $b > $c)){
-    echo 'Vrijednost b je između a i c';
-} else {
-    echo 'Vrijednost b nije između a i c';
+// Ispis prvih deset brojeva
+$a = 1;
+while($a <= 10){
+    echo $a, ' ';
+    $a++;
 }
 
-// Provjera i ispis rezultata
-switch(date('N')){
-    case 1:
-        echo 'Danas je ponedjeljak.';
-        break;
-    case 2:
-        echo 'Danas je utorak.';
-        break;
-    case 3:
-        echo 'Danas je srijeda.';
-        break;
-    case 4:
-        echo 'Danas je četvrtak.';
-        break;
-    case 5:
-        echo 'Danas je petak.';
-        break;
-    case 6:
-        echo 'Danas je subota.';
-        break;
-    case 7:
-        echo 'Danas je nedjelja.';
-        break;
-    default:
-        echo 'Nepostojeći dan.';
-        break;
+echo "\n";
+// Ispis parnih brojeva do 100
+for($i = 1; $i < 100; $i++){
+    if(($i % 2) === 0){
+        echo $i, ' ';
+    }
 }
