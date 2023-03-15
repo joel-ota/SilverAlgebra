c0a6f927167064e9c5db22146d6fdaef2864aabe
Ivan Mandić <ivan.mandic@predavaci.algebra.hr>
2023-03-15 17:55:06 +0000

    Vjezba - rad s datotekama

--- index.php
+++ index.php
@@ -1,16 +1,42 @@
 <?php
+// Čitanje sadržaja datoteke
+$studentsJson = file_get_contents(__DIR__ . '/polaznici.json');
+// Prebacivanje u niz
+$students = json_decode($studentsJson, true);
+?>
 
-// Deklariranje funkcije
-function randomAddition($number){
-    static $a = 0;
-    $a += $number;
-    return $a;
-}
-// Deklariranje funkcije kao varijabla
-$randomAddition = 'randomAddition';
-// Pozivanje funkcije preko varijable te ispis rezultata
-echo $randomAddition(rand(1,10)), "\n";
-echo $randomAddition(rand(1,10)), "\n";
-echo $randomAddition(rand(1,10)), "\n";
-echo $randomAddition(rand(1,10)), "\n";
-echo $randomAddition(rand(1,10)), "\n";
+<!-- Ispis podataka u tablicu -->
+<table border="1" cellpadding="10">
+    <tr>
+        <th>Ime</th> 
+        <th>Prezime</th> 
+        <th>Godine</th> 
+        <th>Email</th> 
+        <th>Telefon</th> 
+    </tr>
+    <?php
+        foreach($students as $student){
+            echo '<tr>';
+            echo '<td>'. $student['name'] .'</td>';
+            echo '<td>'. $student['surname'] .'</td>';
+            echo '<td>'. $student['age'] .'</td>';
+            echo '<td>'. $student['email'] .'</td>';
+            echo '<td>'. $student['phone'] .'</td>';
+            echo '</tr>';
+        }
+    ?>
+</table>
+
+<?php
+// Dodavanje novog polaznika
+$students[] = [
+    "name" => "Iva",        
+    "surname" => "Vukorepa",        
+    "age" => 48,        
+    "email" => "iva.vukorepa",        
+    "phone" => 38597666777
+];
+// Transformiranje u JSON
+$studentsJson = json_encode($students);
+// Zapisivanje novih podataka u datoteku
+file_put_contents(__DIR__ . '/polaznici.json', $studentsJson); 
