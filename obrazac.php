7b939bb30fe8d6c98603dbc175dc22b9caab63ce
Ivan Mandić <ivan.mandic@predavaci.algebra.hr>
2023-04-24 16:01:25 +0000

    Add clean state

--- obrazac.php
+++
@@ -1,6 +0,0 @@
-<form action="obrada.php" method="post" enctype="multipart/form-data">
-    Posalji sliku:
-    <input type="file" name="picture" />
-    <br>
-    <input type="submit" value="Pošalji" />
-</form>
