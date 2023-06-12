bbe241b11629e3dde85dc405d1391d28d6cd8605
Ivan Mandic <ivan.mandic@predavaci.algebra.hr>
2023-06-12 18:57:45 +0000

    dfdf

--- config/routes.php
+++ config/routes.php
@@ -5,13 +5,11 @@ use App\Controller\MediaController;
 use App\Controller\SiteController;
 use Core\Router;
 
-Router::get('/about', function(){
-    return 'About';
-});
+Router::get('/about', [SiteController::class, 'about']);
 
 Router::get('/contact', [SiteController::class, 'contact']);
 
 Router::get('/genres', [GenreController::class, 'index']);
 
 Router::get('/media', [MediaController::class, 'index']);
-
+Router::get('/media/home', [MediaController::class, 'home']);
